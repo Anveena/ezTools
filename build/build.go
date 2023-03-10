4fe7ad765750790bbef662892e1209d276afa53e
潘雨诗 <18993129458@189.cn>
2023-03-10 18:33:03 +0800

    update:go build现在显示更多信息

--- build/build.go
+++ build/build.go
@@ -1,6 +1,7 @@
 package build
 
+// go build -ldflags "-X 'github.com/Anveena/ezTools/build.GitInfo=$(git show -s --format='git版本:%H%n提交者:%an%n提交信息:%s')' -X 'github.com/Anveena/ezTools/build.Date=$(date +'%Y年%m月%d日 %T')'"
 var (
-	GitVersion = ""
-	BuildDate  = ""
+	GitInfo = ""
+	Date    = ""
 )
