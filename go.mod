87af00b368bea2396d36b5ecff6f075a8bb474f8
潘雨诗 <18993129458@189.cn>
2023-06-06 15:56:57 +0800

    update:应该吧panic的权利交给用户

--- go.mod
+++ go.mod
@@ -8,9 +8,9 @@ require (
 	github.com/go-sql-driver/mysql v1.7.1
 	github.com/redis/go-redis/v9 v9.0.2
 	gocv.io/x/gocv v0.32.1
-	golang.org/x/sys v0.7.0
+	golang.org/x/sys v0.8.0
 	golang.org/x/text v0.9.0
-	google.golang.org/grpc v1.54.0
+	google.golang.org/grpc v1.55.0
 	google.golang.org/protobuf v1.30.0
 	gorm.io/driver/mysql v1.4.7
 	gorm.io/gorm v1.24.5
@@ -22,6 +22,7 @@ require (
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/net v0.9.0 // indirect
-	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	google.golang.org/genproto v0.0.0-20230525234025-438c736192d0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230530153820-e85fd2cbaebc // indirect
 )
--- mysql/my_sql.go
+++ mysql/my_sql.go
@@ -15,25 +15,25 @@ type Info struct {
 	DatabaseName      string
 }
 
-func NewDBEngine(dbInfo *Info, gormConfig *gorm.Config, dbModels ...any) *gorm.DB {
+func NewDBEngine(dbInfo *Info, gormConfig *gorm.Config, dbModels ...any) (*gorm.DB, error) {
 	if dbInfo.PasswordBase64Str == "" {
-		panic("密码没配置")
+		return nil, fmt.Errorf("密码没配置")
 	}
 	var pwd string
 	pwd, err := password.Decode(dbInfo.PasswordBase64Str)
 	if err != nil {
-		panic(fmt.Sprintf("密码配的不合适,需要一个神秘的字符串才能解析,错误:\n\t%s", err.Error()))
+		return nil, fmt.Errorf("密码配的不合适,需要一个神秘的字符串才能解析,错误:\n\t%s", err.Error())
 	}
 	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
 		dbInfo.Account, pwd, dbInfo.Host, dbInfo.Port, dbInfo.DatabaseName)
 	db, err := gorm.Open(mysql.Open(dsn), gormConfig)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	for _, ptr := range dbModels {
 		if err = db.Migrator().AutoMigrate(ptr); err != nil {
-			panic(fmt.Sprintf("建表失败!错误:\n\t%s", err.Error()))
+			return nil, fmt.Errorf("建表失败!错误:\n\t%s", err.Error())
 		}
 	}
-	return db
+	return db, nil
 }
